6af8a9ebf4dac0bd81cab2f2afba7ad9e057d0f4
aneesha-13 <aneesha.constella@gmail.com>
2022-02-17 18:55:01 +0530

    Upto Cart feature

---
+++ pages/product/[id].js
@@ -0,0 +1,112 @@
+import React, { useContext, useEffect, useState } from 'react';
+import NextLink from 'next/link';
+import Image from 'next/image';
+import {
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Typography,
+  Card,
+  Button,
+  TextField,
+  CircularProgress,
+} from '@material-ui/core';
+import Layout from '../../component/Layout';
+import useStyles from '../../utils/styles';
+import Product from '../../models/Product';
+import db from '../../utils/db';
+import axios from 'axios';
+import { Store } from '../../utils/store';
+import { useRouter } from 'next/router';
+
+export default function ProductScreen(props) {
+  const router = useRouter();
+  const {dispatch } = useContext(Store);
+//   const { userInfo } = state;
+  const { product } = props;
+  const classes = useStyles();
+
+  const addToCartHandler = async () => {
+    // const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    // const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product , quantity:1} });
+    router.push('/cart');
+  };
+
+  return (
+    <Layout>
+      <div className={classes.section}>
+        <NextLink href="/explore" passHref>
+          <Link>
+            <Typography>back to products</Typography>
+          </Link>
+        </NextLink>
+      </div>
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography component="h5" variant="h5">
+                {product.product}
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Category: {product.category}</Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Price</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>${product.price}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={addToCartHandler}
+                >
+                  Add to cart
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { id } = params;
+
+  await db.connect();
+  const product = await Product.findOne({id}).lean();
+
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
+}
